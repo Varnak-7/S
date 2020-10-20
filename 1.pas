7b92aa341c1541f079959bc88bb3823b7c238d3a
Microtehnika <72962677+Microtehnika@users.noreply.github.com>
2020-10-20 11:55:36 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,20 @@
+﻿program aaa;
+var 
+  a:string;
+  b,c,d:real;
+
+
+  begin
+    writeln('Программа вычисления площади прямоугольника');
+    writeln('Введите значение стороны 1');
+    readln(b);
+    writeln('Введите значение стороны 2');
+    readln(c);
+    d:=b*c;
+    Writeln('Площадь=',d);
+ 
+   
+    
+    
+    
+end.
